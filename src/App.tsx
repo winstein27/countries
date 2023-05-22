2bce063c6a76717e2a91c8ff63187747392a18ff
Winstein Martins <sriwinstein@gmail.com>
2023-05-22 06:59:33 -0300

    :sparkles: feat: add routes
    Adding routes to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import Header from "./components/Header";
+import { RouterProvider } from "react-router-dom";
+
+import router from "./Router";
 
 function App() {
-  return <Header />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Root.tsx
@@ -0,0 +1,7 @@
+import Header from "../components/Header";
+
+const Root = () => {
+  return <Header />;
+};
+
+export default Root;
