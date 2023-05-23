549d1ff1db6f8b31eb8c501385a1d0c4e75a770a
Winstein Martins <sriwinstein@gmail.com>
2023-05-22 22:00:50 -0300

    :sparkles: feat fetch hook
    Adding fetch hook and changing colors

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+const client = axios.create({ baseURL: import.meta.env.VITE_API_URL });
+
+const useFetch = async () => {
+  const [data, setData] = useState<AxiosResponse>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<AxiosError>();
+
+  const fetchData = async () => {
+    try {
+      const response = await client.get("/");
+      setData(response);
+    } catch (error: any) {
+      setError(error);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
