84249b5ea635325f1b2e10f349c23bc649b41422
Winstein Martins <sriwinstein@gmail.com>
2023-05-29 08:50:12 -0300

    :sparkles: feat: resposive detail page
    Making country details page responsive

--- src/components/detail/CountryInfo.tsx
+++ src/components/detail/CountryInfo.tsx
@@ -9,47 +9,47 @@ interface Props {
 const CountryInfo = ({ country }: Props) => {
   return (
     <div className={styles.container}>
-      <img
-        src={country.flag.url}
-        alt={country.flag.alt}
-        className={styles.flag}
-      />
-
-      <h2 className={styles.name}>{country.name}</h2>
-      <div className={styles.infoGroup}>
-        <p>
-          <b>Native name:</b> {country.nativeName}
-        </p>
-        <p>
-          <b>Population:</b>{" "}
-          {new Intl.NumberFormat().format(country.population)}
-        </p>
-        <p>
-          <b>Region:</b> {country.region}
-        </p>
-        <p>
-          <b>Sub Region:</b> {country.subRegion}
-        </p>
-        <p>
-          <b>Capital:</b> {country.capital}
-        </p>
+      <div className={styles.flag}>
+        <img src={country.flag.url} alt={country.flag.alt} />
       </div>
 
-      <div className={styles.infoGroup}>
-        <p>
-          <b>Top Level Domain:</b> {country.domain}
-        </p>
-        <p>
-          <b>Currencies:</b> {country.currencies.join(", ")}
-        </p>
-        <p>
-          <b>Languages:</b> {country.languages.join(", ")}
-        </p>
-      </div>
+      <div className={styles.info}>
+        <h2 className={styles.name}>{country.name}</h2>
+        <div className={styles.infoGroup}>
+          <p>
+            <b>Native name:</b> {country.nativeName}
+          </p>
+          <p>
+            <b>Population:</b>{" "}
+            {new Intl.NumberFormat().format(country.population)}
+          </p>
+          <p>
+            <b>Region:</b> {country.region}
+          </p>
+          <p>
+            <b>Sub Region:</b> {country.subRegion}
+          </p>
+          <p>
+            <b>Capital:</b> {country.capital}
+          </p>
+        </div>
 
-      <div className={styles.infoGroup}>
-        <h2>Border Countries:</h2>
-        {country.borderCountries}
+        <div className={styles.infoGroup}>
+          <p>
+            <b>Top Level Domain:</b> {country.domain}
+          </p>
+          <p>
+            <b>Currencies:</b> {country.currencies.join(", ")}
+          </p>
+          <p>
+            <b>Languages:</b> {country.languages.join(", ")}
+          </p>
+        </div>
+
+        <div className={styles.infoGroup}>
+          <h2>Border Countries:</h2>
+          {country.borderCountries}
+        </div>
       </div>
     </div>
   );
